9796efba873019f9ba3b65b9c801fc81daa24826
YunfeiZHAO <moizhaoyunfei@gmail.com>
2021-03-12 21:30:13 +0100

    fixed

--- utils/general.py
+++ utils/general.py
@@ -573,3 +573,4 @@ def increment_path(path, exist_ok=True, sep=''):
         matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
         i = [int(m.groups()[0]) for m in matches if m]  # indices
         n = max(i) + 1 if i else 2  # increment number
+        return f"{path}{sep}{n}"  # update path
